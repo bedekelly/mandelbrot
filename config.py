35d62466ddb2fab89cdf2ea209f727848d07925d
Bede Kelly <bedekelly97@gmail.com>
2015-11-20 07:43:05 +0000

    Add julia constant to config

--- config.py
+++ config.py
@@ -3,8 +3,16 @@ config.py
 Describe the image size, iteration depth and location on the fractal to render.
 """
 
-MAX_X, MAX_Y = 5000, 5000
+MAX_X, MAX_Y = 32000, 18000
 MAX_ITERATION = 255
-X1, X2, Y1, Y2 = -2.5, -0.5, -1, 1
+X1, X2, Y1, Y2 = -1.8, 0.2, -1, 1
 # Seahorses:
 # X1, X2, Y1, Y2 = -0.755, -0.745, 0.09, 0.11
+
+# Julia set constant:
+C = (-0.8, 0.156)
+#C = -0.835, -0.2321
+#C = 0.687, 0.312
+#C = -0.70176, 0.3842
+#C = -0.4, 0.6
+
